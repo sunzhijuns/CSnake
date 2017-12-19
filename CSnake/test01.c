40df1e49dabc57f6c2ab9452a77b0f6106ed1419
sunzhijuns <2843359242@qq.com>
2017-12-20 03:50:55 +0800

    形参中数组不是数组，是指针变量

--- CSnake/test01.c
+++ CSnake/test01.c
@@ -3,52 +3,29 @@
 #include<stdlib.h>
 #include<string.h>
 
-int * func()
-{
-	int * p;
-	/*
-	1、p指向堆区空间，堆区空间需要调用malloc函数返回
-	2、只要程序不结束，用户不free，堆区空间一直存在
-	3、int * 指向int，所以，需要分配sizeof(int)大小的空间
-	功能：在堆区分配一块连续空间
-	参数：需要分配空间的大小
-	返回值：成功返回空间的首地址，失败返回NULL
-	*/
-	p = (int *)malloc(sizeof(int));
+//形参中数组不是数组，是指针变量
+/*
+下面4中写法完全一样，最后写指针变量的形式
+void func(int a[10])
+void func(int a[10000])
+void func(int a[])
+*/
+void func(int * a) {
+	printf("func sizeof(a) = %d\n", sizeof(a));
 
-	return p;
-}
-
-int * func1()
-{
-	int a = 10;
-	return a;
+	int b = 1;
+	a = &b;
+	*a = 22;
+	printf("b = %d\n", b);
 }
 
 int main(void) {
-	
 
-	int * p;
-	p = func();
-	if (p == NULL)//说明分配失败
-	{
-		printf("p == NULL\n");
-		return -1;
-	}
-	*p = 111;//内存出问题
-	int a = func1();
-	int b = func1();
-	int c = a + b;
-	printf("%d,%d\n", *p,c);
+	int a[10] = {1,2,3,4,5,6,7,8,9,0};
 
-	//使用完成，堆区空间需要手动释放
-	/*
-		1、一夫一妻制，一个malloc一个free
-		2、free(p)不是释放p的空间，是释放p所指向的空间
-		3、free完的空间，不能在使用，同一块内存只能释放一次
-	*/
-	free(p);
-	p = NULL;
+	printf("main sizeof(a) = %d\n", sizeof(a));
+
+	func(a);
 
 
 	system("pause");//暂停	#include<stdlib.h>
