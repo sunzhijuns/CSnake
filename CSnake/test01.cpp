8c7723fb0490178b3af81674fa0311f4e38ca323
sunzhijuns <2843359242@qq.com>
2017-12-19 20:54:50 +0800

    初始化

---
+++ CSnake/test01.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void) {
+
+	printf("12345\n");
+	system("pause");//暂停	#include<stdlib.h>
+	//2.ctrl+f5
+	return 0;
+}
