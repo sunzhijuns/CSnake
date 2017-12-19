4ea13c4413abfb61477c9739cb01979d65d5a09f
sunzhijuns <2843359242@qq.com>
2017-12-20 02:33:16 +0800

    不可以返回局部变量的地址

--- CSnake/test01.c
+++ CSnake/test01.c
@@ -3,39 +3,34 @@
 #include<stdlib.h>
 #include<string.h>
 
+int * func()
+{
+	int a = 10;
+	/*
+	func调用完毕，a自动释放，所谓释放，不是空间消失，
+	a的空间被系统回收，用户不能操作
+
+	*/
+	return &a;
+}
+
+int * func1()
+{
+	int a = 10;
+	return a;
+}
+
 int main(void) {
-
-	{
-		/*
-		野指针
-		1、定义变量，&变量，这些都是合法地址，也叫合法指针
-		2、通过malloc()等堆区分配空间返回的地址，也是合法地址
-		3、除了1、2，非法地址，野地址，野指针
-		*/
-		int a = 10;
-		int * p;//未初始化，为 野指针
-				//*p = &a;// 这句话导致内存问题，操作野指针所指向的内存
-		p = &a;
 	
-	}
 
-	{
-		/*
-		1、所谓的空指针，只是为了做一个标志
-		2、给这个指针变量赋值为NULL，NULL就是数字0
-		*/
-		int * p = NULL;
+	int * p;
+	p = func();
+	*p = 111;//内存出问题
+	int a = func1();
+	int b = func1();
+	int c = a + b;
+	printf("%d,%d\n", *p,c);
 
-		if (p == NULL)//说明这个指针，没有使用
-		{
-			int a = 10;
-			p = &a;
-		}
-
-		// 不要操作空指针所指向的空间
-		int * tmp = NULL;
-		//*temp = 111; // 空指针只是做标志位，标志这个变量没有人用
-	}
 
 	system("pause");//暂停	#include<stdlib.h>
 	//2.ctrl+f5
