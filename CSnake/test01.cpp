d926926a69c34b06f3675173c7853290fba9af27
sunzhijuns <2843359242@qq.com>
2017-12-19 21:03:22 +0800

    开始数组

--- CSnake/test01.cpp
+++ CSnake/test01.cpp
@@ -4,7 +4,18 @@
 #include<string.h>
 
 int main(void) {
+	
+	int id1 = 1;
+	int id2 = 2;
+	int id3 = 3;
 
+	int id[10];
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		id[i] = i + 1;
+		printf("id[%d] = %d\n", i, id[i]);
+	}
 	printf("12345\n");
 	system("pause");//暂停	#include<stdlib.h>
 	//2.ctrl+f5
