c24872a99eb2bf698a6ceafb1ec75e7c01c48ee1
sunzhijuns <2843359242@qq.com>
2017-12-21 01:06:18 +0800

    键盘按下

--- CSnake/test01.c
+++ CSnake/test01.c
@@ -1,14 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include<stdlib.h>
 #include <string.h>
+#include<time.h>
+#include<conio.h>//_getch(),_kbhit
+#include<Windows.h>//Sleep()
 
-/*
-
-在C中，调用函数时，前面没有定义，别的地方有定义，不用声明，可以调用，有警告
-在c++中，调用函数时，前面没有定义，别的地方有定义，需要声明后，才可调用
-*/
 int main(void) {
-	printf("%d\n", strlen("abc"));
+
+	//1、设置一个种子
+	//srand(100);
+	srand(time(NULL));
+	//2、生成随机数
+	int i = 0;
+	int num;
+	for ( i = 0; i < 10; i++)
+	{
+		num = rand();
+		printf("%d\n", num);
+
+	}
+	char ch = _getch();
+	printf("%d,%c\n", ch, ch);
+
+	while (1)
+	{
+		while (_kbhit())
+		{
+
+			printf("有键盘按下\n");
+			Sleep(1000);
+		}
+	}
+
 	return 0;
 }
