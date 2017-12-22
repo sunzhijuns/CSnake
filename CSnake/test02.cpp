0f0627f1a67456c40d21160ae06f02ece8ee8ad3
sunzhijuns <2843359242@qq.com>
2017-12-22 18:04:58 +0800

    测试

--- CSnake/test02.cpp
+++ CSnake/test02.cpp
@@ -44,120 +44,125 @@ stringmap merge(stringmap a, stringmap b) {
 //int test_unordered_map2(void)
 int main(void)
 {
-	////////// at/size
-	std::unordered_map<std::string, int> mymap = { { "Mars", 3000 },{ "Saturn", 60000 },{ "Jupiter", 70000 } };
+	//////////// at/size
+	//std::unordered_map<std::string, int> mymap = { { "Mars", 3000 },{ "Saturn", 60000 },{ "Jupiter", 70000 } };
 
-	mymap.at("Mars") = 3396;
-	mymap.at("Saturn") += 272;
-	mymap.at("Jupiter") = mymap.at("Saturn") + 9638;
+	//mymap.at("Mars") = 3396;
+	//mymap.at("Saturn") += 272;
+	//mymap.at("Jupiter") = mymap.at("Saturn") + 9638;
 
-	for (auto& x : mymap) {
-		std::cout << x.first << ": " << x.second << std::endl;
-	}
+	//for (auto& x : mymap) {
+	//	std::cout << x.first << ": " << x.second << std::endl;
+	//}
 
-	std::cout << "mymap.size() is " << mymap.size() << std::endl;
-	/*
-	
-	
-	/////////// begin
-	std::unordered_map<std::string, std::string> mymap2 = { { "Australia", "Canberra" },{ "U.S.", "Washington" },{ "France", "Paris" } };
+	//std::cout << "mymap.size() is " << mymap.size() << std::endl;
+	//
+	/////////////// begin
+	//std::unordered_map<std::string, std::string> mymap2 = { 
+	//	{"1","1"},{ "11","11" },{ "111","111" },{ "12","12" },
+	//	{ "Australia", "Canberra" },{ "U.S.", "Washington" },{ "France", "Paris" } ,
+	//	{ "aaa", "111" },{ "bbb", "222" },{ "ccc", "333" },
+	//	{ "ddd", "444" },{ "eee", "555" },{ "fff", "666" },
+	//};
 
-	std::cout << "mymap2 contains:";
-	for (auto it = mymap2.begin(); it != mymap2.end(); ++it)
-		std::cout << " " << it->first << ":" << it->second;
-	std::cout << std::endl;
+	//std::cout << "mymap2 contains:";
+	//for (auto it = mymap2.begin(); it != mymap2.end(); ++it)
+	//	std::cout << " " << it->first << ":" << it->second;
+	//std::cout << std::endl;
 
-	std::cout << "mymap2's buckets contain:\n";
-	for (unsigned i = 0; i < mymap2.bucket_count(); ++i) {
-		std::cout << "bucket #" << i << " contains:";
-		for (auto local_it = mymap2.begin(i); local_it != mymap2.end(i); ++local_it)
-			std::cout << " " << local_it->first << ":" << local_it->second;
-		std::cout << std::endl;
-	}
+	//std::cout << "mymap2's buckets contain:\n";
+	//for (unsigned i = 0; i < mymap2.bucket_count(); ++i) {
+	//	std::cout << "bucket #" << i << " contains:";
+	//	for (auto local_it = mymap2.begin(i); local_it != mymap2.end(i); ++local_it)
+	//		std::cout << " " << local_it->first << ":" << local_it->second;
+	//	std::cout << std::endl;
+	//}
 
-	////////////// bucket
-	std::unordered_map<std::string, std::string> mymap3 = {
-		{ "us", "United States" },
-		{ "uk", "United Kingdom" },
-		{ "fr", "France" },
-		{ "de", "Germany" }
-	};
+	//////////////// bucket
+	//std::unordered_map<std::string, std::string> mymap3 = {
+	//	{ "us", "United States" },
+	//	{ "uk", "United Kingdom" },
+	//	{ "fr", "France" },
+	//	{ "de", "Germany" }
+	//};
 
-	for (auto& x : mymap3) {
-		std::cout << "Element [" << x.first << ":" << x.second << "]";
-		std::cout << " is in bucket #" << mymap3.bucket(x.first) << std::endl;
-	}
+	//for (auto& x : mymap3) {
+	//	std::cout << "Element [" << x.first << ":" << x.second << "]";
+	//	std::cout << " is in bucket #" << mymap3.bucket(x.first) << std::endl;
+	//}
 
-	/////////////// count
-	std::unordered_map<std::string, double> mymap4 = {
-		{ "Burger", 2.99 },
-		{ "Fries", 1.99 },
-		{ "Soda", 1.50 } };
+	///////////////// count
+	//std::unordered_map<std::string, double> mymap4 = {
+	//	{ "Burger", 2.99 },
+	//	{ "Fries", 1.99 },
+	//	{ "Soda", 1.50 } };
 
-	for (auto& x : { "Burger", "Pizza", "Salad", "Soda" }) {
-		if (mymap4.count(x)>0)
-			std::cout << "mymap4 has " << x << std::endl;
-		else
-			std::cout << "mymap4 has no " << x << std::endl;
-	}
+	//for (auto& x : { "Burger", "Pizza", "Salad", "Soda" }) {
+	//	if (mymap4.count(x) > 0){
+	//		std::cout << mymap4.count(x) << "  ";
+	//		std::cout << "mymap4 has " << x << std::endl;
+	//	}
+	//	else
+	//		std::cout << "mymap4 has no " << x << std::endl;
+	//}
 
-	///////////////// erase
-	std::unordered_map<std::string, std::string> mymap5;
+	/////////////////// erase
+	//std::unordered_map<std::string, std::string> mymap5;
 
-	// populating container:
-	mymap5["U.S."] = "Washington";
-	mymap5["U.K."] = "London";
-	mymap5["France"] = "Paris";
-	mymap5["Russia"] = "Moscow";
-	mymap5["China"] = "Beijing";
-	mymap5["Germany"] = "Berlin";
-	mymap5["Japan"] = "Tokyo";
+	//// populating container:
+	//mymap5["U.S."] = "Washington";
+	//mymap5["U.K."] = "London";
+	//mymap5["France"] = "Paris";
+	//mymap5["Russia"] = "Moscow";
+	//mymap5["China"] = "Beijing";
+	//mymap5["Germany"] = "Berlin";
+	//mymap5["Japan"] = "Tokyo";
 
-	// erase examples:
-	mymap5.erase(mymap5.begin());      // erasing by iterator
-	mymap5.erase("France");             // erasing by key
-	mymap5.erase(mymap5.find("China"), mymap5.end()); // erasing by range
+	//// erase examples:
+	//mymap5.erase(mymap5.begin());      // erasing by iterator
+	//mymap5.erase("France");             // erasing by key
+	//mymap5.erase(mymap5.find("China"), mymap5.end()); // erasing by range
 
-													  // show content:
-	for (auto& x : mymap5)
-		std::cout << x.first << ": " << x.second << std::endl;
+	//												  // show content:
+	//for (auto& x : mymap5)
+	//	std::cout << x.first << ": " << x.second << std::endl;
 
-	////////////////////// find
-	std::unordered_map<std::string, double> mymap6 = {
-		{ "mom", 5.4 },
-		{ "dad", 6.1 },
-		{ "bro", 5.9 } };
+	//////////////////////// find
+	//std::unordered_map<std::string, double> mymap6 = {
+	//	{ "mom", 5.4 },
+	//	{ "dad", 6.1 },
+	//	{ "bro", 5.9 } };
 
-	std::string input;
-	std::cout << "who? ";
-	getline(std::cin, input);
+	//std::string input;
+	//std::cout << "who? ";
+	//getline(std::cin, input);
 
-	std::unordered_map<std::string, double>::const_iterator got = mymap6.find(input);
+	//std::unordered_map<std::string, double>::const_iterator got = mymap6.find(input);
 
-	if (got == mymap6.end())
-		std::cout << "not found";
-	else
-		std::cout << got->first << " is " << got->second;
+	//if (got == mymap6.end())
+	//	std::cout << "not found";
+	//else
+	//	std::cout << got->first << " is " << got->second;
 
-	std::cout << std::endl;
+	//std::cout << std::endl;
 
-	//////////////////// insert
-	std::unordered_map<std::string, double>
-		myrecipe,
-		mypantry = { { "milk", 2.0 },{ "flour", 1.5 } };
+	////////////////////// insert
+	//std::unordered_map<std::string, double>
+	//	myrecipe,
+	//	mypantry = { { "milk", 2.0 },{ "flour", 1.5 } };
 
-	std::pair<std::string, double> myshopping("baking powder", 0.3);
+	//std::pair<std::string, double> myshopping("baking powder", 0.3);
 
-	myrecipe.insert(myshopping);                        // copy insertion
-	myrecipe.insert(std::make_pair<std::string, double>("eggs", 6.0)); // move insertion
-	myrecipe.insert(mypantry.begin(), mypantry.end());  // range insertion
-	myrecipe.insert({ { "sugar", 0.8 },{ "salt", 0.1 } });    // initializer list insertion
+	//myrecipe.insert(myshopping);                        // copy insertion
+	//myrecipe.insert(std::make_pair<std::string, double>("eggs", 6.0)); // move insertion
+	//myrecipe.insert(mypantry.begin(), mypantry.end());  // range insertion
+	//myrecipe.insert({ { "sugar", 0.8 },{ "salt", 0.1 } });    // initializer list insertion
 
-	std::cout << "myrecipe contains:" << std::endl;
-	for (auto& x : myrecipe)
-		std::cout << x.first << ": " << x.second << std::endl;
+	//std::cout << "myrecipe contains:" << std::endl;
+	//for (auto& x : myrecipe)
+	//	std::cout << x.first << ": " << x.second << std::endl;
 
-	std::cout << std::endl;
+	//std::cout << std::endl;
 
 	//////////////////// =
 	stringmap first = { { "AAPL", "Apple" },{ "MSFT", "Microsoft" } };  // init list
@@ -168,7 +173,7 @@ int main(void)
 	std::cout << "first contains:";
 	for (auto& elem : first) std::cout << " " << elem.first << ":" << elem.second;
 	std::cout << std::endl;
-	*/
+	//
 	return 0;
 }
 
